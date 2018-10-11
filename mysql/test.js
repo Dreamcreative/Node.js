3e734eb33e44e863a5a6f8be512c229b67f820e8
马啸 <m543438924@163.com>
2018-10-11 23:27:39 +0800

    review & add yarn

--- mysql/test.js
+++ mysql/test.js
@@ -7,8 +7,8 @@ var connection = mysql.createConnection({
 });
  
 connection.connect();
- 
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
+  if (error) console.log( error );
+  // console.log('The solution is: ', results[0].solution);
 });
+connection.end();
